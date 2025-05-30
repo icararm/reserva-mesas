de15da24a12fa7b3271dfeaed93b163c7e6ef3cd
Icara da Rocha Marques <icararm123@gmail.com>
2025-05-29 21:55:34 -0300

    alteração no app.js

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 import express from 'express';
-import mesasRoutes from './routes/mesasRoutes.js';
-import relatoriosRoutes from './routes/relatoriosRoutes.js';
-import reservasRoutes from './routes/reservasRoutes.js';
-import usuariosRoutes from './routes/usuariosRoutes.js';
-
-const app = express();
-app.use(express.json());
-
-app.use('/mesas', mesasRoutes);      
-app.use('/reservas', reservasRoutes); 
-app.use('/usuarios', usuariosRoutes); 
-app.use('/relatorios', relatoriosRoutes); 
-
 import path from 'path';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 
+// Importação das rotas
+import atendenteRoutes from './routes/atendente.js';
+import garcomRoutes from './routes/garcom.js';
+import gerenteRoutes from './routes/gerente.js';
+
+// Configurações de diretório
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+const app = express();
 const PORT = 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json()); // Parse JSON
+app.use(express.urlencoded({ extended: true })); // Parse form data
+
+app.use(express.static('public'));
+
+// Rotas
+app.use('/api/atendente', atendenteRoutes);
+app.use('/api/garcom', garcomRoutes);
+app.use('/api/gerente', gerenteRoutes);
+
+
+// Iniciar servidor
 app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT} http://localhost:3000`);
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
-
-  
